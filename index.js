65a0541344cea26a4a4882f78927a226b67f76dd
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-06-05 15:16:23 -0400

    Add shuffle code

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const answers = [
-  "Number",
-  "Boolean",
-  "Null",
-  "Undefined",
-  "BigInt",
-  "String",
-  "Symbol",
-  "Object",
-  "Function",
-  "Currency"
+  'Number',
+  'Boolean',
+  'Null',
+  'Undefined',
+  'BigInt',
+  'String',
+  'Symbol',
+  'Object',
+  'Function',
+  'Currency'
 ];
 
+// Create a new empty array
+const shuffled = [];
+
+// loop over the answers array
+for (let i = 0; i < answers.length; i++) {
+  //generate a random index for shuffled
+  const index = Math.floor(Math.random() * (shuffled.length + 1));
+  //insert current element at index
+  shuffled.splice(index, 0, answers[i]);
+}
+
 // get the answers element
-const answerDiv = document.querySelector("#answers");
+const answerDiv = document.querySelector('#answers');
 
 // for each element in the array, append a node to the DOM
-for (let i = 0; i < answers.length; i++) {
+for (let i = 0; i < shuffled.length; i++) {
   // create a DOM element
-  const ans = document.createElement("li");
-  ans.innerHTML = answers[i];
-  ans.className = "answer";
+  const ans = document.createElement('li');
+  ans.innerHTML = shuffled[i];
+  ans.className = 'answer';
   answerDiv.append(ans);
 }
