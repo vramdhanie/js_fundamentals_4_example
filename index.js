f6d4581f1f52644bb3eabe3ac8ff240694df0a3f
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-06-06 22:10:42 -0400

    Refactor to use map

--- index.js
+++ index.js
@@ -25,11 +25,8 @@ for (let i = 0; i < answers.length; i++) {
 // get the answers element
 const answerDiv = document.querySelector('#answers');
 
-// for each element in the array, append a node to the DOM
-for (let i = 0; i < shuffled.length; i++) {
-  // create a DOM element
-  const ans = document.createElement('li');
-  ans.innerHTML = shuffled[i];
-  ans.className = 'answer';
-  answerDiv.append(ans);
-}
+const elements = shuffled.map(function(answer) {
+  return '<li class="answer">' + answer + '</li>';
+});
+
+answerDiv.innerHTML = elements.join('');
